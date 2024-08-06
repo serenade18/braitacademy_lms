2b1a487ecc577754682d8353686c21cf17f12c3e
Ahmed Sereta <seretaahmed18@gmail.com>
2024-08-06 18:23:21 +0300

    removed the sidebar filter

--- resources/views/web/default/pages/classes.blade.php
+++ resources/views/web/default/pages/classes.blade.php
@@ -38,12 +38,12 @@
                 @include('web.default.pages.includes.top_filters')
 
                 <div class="row mt-20">
-                    <div class="col-12 col-lg-8">
+                    <div class="col-12 col-lg-12">
 
                         @if(empty(request()->get('card')) or request()->get('card') == 'grid')
                             <div class="row">
                                 @foreach($webinars as $webinar)
-                                    <div class="col-12 col-lg-6 mt-20">
+                                    <div class="col-12 col-lg-4 mt-20">
                                         @include('web.default.includes.webinar.grid-card',['webinar' => $webinar])
                                     </div>
                                 @endforeach
@@ -59,7 +59,7 @@
                     </div>
 
 
-                    <div class="col-12 col-lg-4">
+                    <!-- <div class="col-12 col-lg-2">
                         <div class="mt-20 p-20 rounded-sm shadow-lg border border-gray300 filters-container">
 
                             <div class="">
@@ -84,26 +84,11 @@
                                 </div>
                             </div>
 
-                            <!-- <div class="mt-25 pt-25 border-top border-gray300">
-                                <h3 class="category-filter-title font-20 font-weight-bold text-dark-blue">{{ trans('site.more_options') }}</h3>
-
-                                <div class="pt-10">
-                                    @foreach(['subscribe','certificate_included','with_quiz','featured'] as $moreOption)
-                                        <div class="d-flex align-items-center justify-content-between mt-20">
-                                            <label class="cursor-pointer" for="filterLanguage{{ $moreOption }}">{{ trans('webinars.show_only_'.$moreOption) }}</label>
-                                            <div class="custom-control custom-checkbox">
-                                                <input type="checkbox" name="moreOptions[]" id="filterLanguage{{ $moreOption }}" value="{{ $moreOption }}" @if(in_array($moreOption, request()->get('moreOptions', []))) checked="checked" @endif class="custom-control-input">
-                                                <label class="custom-control-label" for="filterLanguage{{ $moreOption }}"></label>
-                                            </div>
-                                        </div>
-                                    @endforeach
-                                </div>
-                            </div> -->
 
 
                             <button type="submit" class="btn btn-sm btn-primary btn-block mt-30">{{ trans('site.filter_items') }}</button>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
 
             </form>
